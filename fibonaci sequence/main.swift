d6885fd138e84ce054a88b8cc989ef5ff09c03ba
Ashlyjayakrishnan <ashlybaby95@gmail.com>
2019-02-12 13:25:29 -0500

    Initial Commit

---
+++ fibonaci sequence/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fibonaci sequence
+//
+//  Created by Ashly Baby on 2019-02-12.
+//  Copyright © 2019 Ashly Baby. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
